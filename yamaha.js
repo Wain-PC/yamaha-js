1e040849b69de49bd38ccd62f983a289d6783111
Pavel Dranichnikov <dranichnikov@teletrade.ru>
2015-12-14 19:42:34 +0300

    Major methods refactor - part one.

--- yamaha.js
+++ yamaha.js
@@ -6,74 +6,50 @@
 /**
  * The Yamaha Module Constructor.
  * @constructor
- * @param {string} ip - The ip of the yamaha receiver.
- * @param {number} responseDelay - The delay of the response for put commands, in seconds - defaults to 1. Better than polling...
+ * @param {object} options - Options object
+ * @param {string} options.ip - The ip of the yamaha receiver.
+ * @param {string} options.responseDelay - The delay of the response for put commands, in seconds - defaults to 1. Better than polling...
+ * @param {string} options.zones - Number of zones supported by receiver, including main zone
+ * @param {string} options.activeZone - Currently active zone
  *
  */
-function Yamaha(ip, responseDelay) {
-    if (typeof responseDelay == 'string' || responseDelay instanceof String) responseDelay = parseInt(responseDelay);
-    if (!responseDelay) responseDelay = 1;
-    this.ip = ip;
-    this.responseDelay = responseDelay;
+function Yamaha(options) {
+    this.ip = '192.168.1.2';
+    this.zones = 2;
+    this.activeZone = 1;
+    this.responseDelay = 0;
+
+    for (var param in options) {
+        if (options.hasOwnProperty(param) && (this.hasOwnProperty(param))) {
+            this[param] = options[param];
+        }
+    }
+
 }
 
-Yamaha.prototype.powerOn = function () {
-    var self = this;
-    //the receiver needs to be powered on
-    //what we should do here is check if it's already on
-    return this.isOn().then(function (isOn) {
-        if(isOn) {
-            return false;
-        }
-        var command = '<YAMAHA_AV cmd="PUT"><Main_Zone><Power_Control><Power>On</Power></Power_Control></Main_Zone></YAMAHA_AV>';
-        return self.SendXMLToReceiver(command);
-    });
+/*----------Service methods----------*/
 
-};
-
-Yamaha.prototype.powerOff = function (to) {
-    var command = '<YAMAHA_AV cmd="PUT"><Main_Zone><Power_Control><Power>Standby</Power></Power_Control></Main_Zone></YAMAHA_AV>';
-    return this.SendXMLToReceiver(command);
-};
-
-
-Yamaha.prototype.setVolumeTo = function (to) {
-    var command = '<YAMAHA_AV cmd="PUT"><Main_Zone><Volume><Lvl><Val>' + to + '</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume></Main_Zone></YAMAHA_AV>';
-    return this.SendXMLToReceiver(command);
-};
-
-Yamaha.prototype.volumeUp = function (by) {
-    return this.adjustVolumeBy(by);
-};
-
-Yamaha.prototype.volumeDown = function (by) {
-    return this.adjustVolumeBy(-by);
-};
-
-Yamaha.prototype.adjustVolumeBy = function (by) {
-    var self = this;
-    //what we need to do here is get the basic info about the receiver, then change the volume by the given number
-    //number can be either positive or negative
-    if (typeof by == 'string' || by instanceof String) {
-        by = parseInt(by);
+Yamaha.prototype.getZoneName = function (zoneNumber) {
+    if(zoneNumber > 1) {
+        return 'Zone_'+zoneNumber;
     }
-    //return instantly resolved promise for chaining if the delta is zero or NaN (or any other bad value)
-    if(!by || !by instanceof Number) {
-        return new Promise().resolve();
-    }
-
-    return this.getBasicInfo().then(function (basicInfo) {
-        return self.setVolumeTo(basicInfo.getVolume() + by);
-    });
+    return 'Main_Zone';
 };
 
-Yamaha.prototype.setMainInputTo = function (to) {
-    return this.setInputTo("Main_Zone", to);
+Yamaha.prototype.activeZoneName = function () {
+    return this.getZoneName(this.activeZone);
 };
 
-Yamaha.prototype.setInputTo = function (zone, to) {
-    var command = '<YAMAHA_AV cmd="PUT"><' + zone + '><Input><Input_Sel>' + to + '</Input_Sel></Input></' + zone + '></YAMAHA_AV>';
-    return this.SendXMLToReceiver(command);
+Yamaha.prototype.createCommand = function (type, container, command) {
+    return '<YAMAHA_AV cmd="'+type+'"><' + container + '>' + command + '</' + container + '></YAMAHA_AV>';
+};
+
+Yamaha.prototype.createZoneCommand = function (type, zone, command) {
+    return this.createCommand(type, this.getZoneName(zone), command);
+};
+
+Yamaha.prototype.createActiveZoneCommand = function (type, command) {
+    return this.createZoneCommand(this.activeZone, command);
 };
 
 Yamaha.prototype.SendXMLToReceiver = function (bodyText) {
@@ -87,10 +63,10 @@ Yamaha.prototype.SendXMLToReceiver = function (bodyText) {
             if (this.readyState == 4) {
                 if (this.status == 200) {
                     var responseText = this.responseText;
-                    if(isPutCommand) {
+                    if(isPutCommand && _self.responseDelay) {
                         setTimeout(function () {
                             resolve(xmlToJSON.parseString(responseText));
-                        }, _self.responseDelay * 1000);
+                        }, _self.responseDelay);
                     }
                     else {
                         resolve(this.responseText);
@@ -113,84 +89,270 @@ Yamaha.prototype.SendXMLToReceiver = function (bodyText) {
     });
 };
 
-Yamaha.prototype.getBasicInfo = function () {
+/**
+ * long polling of any request made to the receiver
+ * @param YamahaCall - the metod name to be called
+ * @param parameter - argument to pass to the above method
+ * @param expectedReturnValue - stop polling when this value meets our requirements
+ * @returns {Promise}
+ */
+Yamaha.prototype.when = function (YamahaCall, parameter, expectedReturnValue) {
+    var self = this;
+    return new Promise(function (resolve, reject) {
+        var tries = 0;
+        var interval = setInterval(function () {
+            self[YamahaCall](parameter).then(function (result) {
+                if (result == expectedReturnValue) {
+                    clearInterval(interval);
+                    resolve();
+                }
+                tries++;
+                if (tries > 40) reject("Timeout");
+            });
 
-    var command = '<YAMAHA_AV cmd="GET"><Main_Zone><Basic_Status>GetParam</Basic_Status></Main_Zone></YAMAHA_AV>';
-    return this.SendXMLToReceiver(command).then(function (result) {
-        return enrichBasicStatus(result);
+        }, 500);
     });
-
 };
 
-function enrichBasicStatus(basicStatus) {
-    //I assume YAMAHA_AV can be either object or array (depending on the receiver model)
-    //let's check it out and act accordingly
-    var yamahaAv = basicStatus.YAMAHA_AV[0];
 
-    basicStatus.getVolume = function () {
-        return parseInt(yamahaAv.Main_Zone[0].Basic_Status[0].Volume[0].Lvl[0].Val[0]._text);
-    };
+/*----------Service methods end----------*/
 
-    basicStatus.isMuted = function () {
-        return yamahaAv.Main_Zone[0].Basic_Status[0].Volume[0].Mute[0]._text !== "Off";
-    };
 
-    basicStatus.isOn = function () {
-        return yamahaAv.Main_Zone[0].Basic_Status[0].Power_Control[0].Power[0]._text === "On";
-    };
+/**
+ * Switches power of some receiver zone
+ * @param zone {Number} - # of zone to switch
+ * @param powerOn {Boolean} - if true, turn the receiver on, else turn off
+ * @returns {Promise}
+ */
+Yamaha.prototype.switchPower = function (zone, powerOn) {
+    var self = this,
+        powerStatus = 'Standby';
 
-    basicStatus.isOff = function () {
-        return !basicStatus.isOn();
-    };
+    if(powerOn === false) {
+        powerStatus = 'On';
+    }
 
-    basicStatus.getCurrentInput = function () {
-        return yamahaAv.Main_Zone[0].Basic_Status[0].Input[0].Input_Sel[0]._text;
-    };
-
-    basicStatus.isPartyModeEnabled = function () {
-        var basicStatus = yamahaAv.Main_Zone[0].Basic_Status[0];
-        try {
-            return yamahaAv.Main_Zone[0].Basic_Status[0].Party_Info[0]._text === "On";
+    if(!zone) {
+        zone = this.activeZone;
+    }
+    //the receiver needs to be powered on
+    //what we should do here is check if it's already on
+    return this.isOn(zone).then(function (isOn) {
+        //if the receiver is on while we're trying to turn it on, or
+        //if the receiver is off while we're trying to turn it off
+        if((isOn && powerOn) || (!isOn && !powerOn)) {
+            return isOn;
         }
-        //this means getting the property of undefined, so the party mode is not supported by the receiver
-        catch (err) {
-            return false;
-        }
-    };
+        var command = self.createZoneCommand('PUT', zone, '<Power_Control><Power>'+ powerStatus +'</Power></Power_Control>');
+        return self.SendXMLToReceiver(command);
+    });
+};
 
-    basicStatus.isPureDirectEnabled = function () {
-        console.log(yamahaAv.Main_Zone[0].Basic_Status[0].Sound_Video[0].Pure_Direct[0].Mode[0]._text);
-        return yamahaAv.Main_Zone[0].Basic_Status[0].Sound_Video[0].Pure_Direct[0].Mode[0]._text === "On";
-    };
-    return basicStatus;
-}
+/**
+ * Power on some receiver zone
+ * @param zone {Boolean} - # of zone to turn on
+ * @returns {Promise}
+ */
+Yamaha.prototype.powerOn = function (zone) {
+    return this.switchPower(zone, true);
+};
 
+/**
+ * Power off some receiver zone
+ * @param zone {Boolean} - # of zone to turn on
+ * @returns {Promise}
+ */
+Yamaha.prototype.powerOff = function (zone) {
+    return this.switchPower(zone, false);
+};
 
-// Add direct functions for basic info
-function addBasicInfoWrapper(basicInfoName) {
-    Yamaha.prototype[basicInfoName] = function () {
-        return this.getBasicInfo().then(function(basicInfo) {
-            return basicInfo[basicInfoName]();
-        });
-    };
-}
-//TODO: no list, take properties of basicStatus object
-var basicInfos = ["getVolume", "isMuted", "isOn", "isOff", "getCurrentInput", "isPartyModeEnabled", "isPureDirectEnabled"],
-    i, basicInfo;
-for (i = 0; i < basicInfos.length; i++) {
-    basicInfo = basicInfos[i];
-    addBasicInfoWrapper(basicInfo);
-}
+/**
+ * Power on all receiver zones
+ * @returns {Promise}
+ */
+Yamaha.prototype.powerOnAll = function () {
+    var self = this,
+        i, promise;
+    //loop through all zones and power them on (one by one, NOT simultaneously)
+    promise = this.powerOn(1);
+    for(i=2;i<=this.zones;i++) {
+        promise = promise.then(this.powerOn(i));
+    }
+    return promise;
+};
 
-Yamaha.prototype.getSystemConfig = function () {
-    var command = '<YAMAHA_AV cmd="GET"><System><Config>GetParam</Config></System></YAMAHA_AV>';
+/**
+ * Power off all receiver zones
+ * @returns {Promise}
+ */
+Yamaha.prototype.powerOffAll = function () {
+    var self = this,
+        i, promise;
+    //loop through all zones and power them on (one by one, NOT simultaneously)
+    promise = this.powerOff(1);
+    for(i=2;i<=this.zones;i++) {
+        promise = promise.then(this.powerOff(i));
+    }
+    return promise;
+};
+
+/**
+ *
+ * @param to {Number} change volume of the active zone to value (generally from -800 (min - -80.0 db) to 165 (+16.5 db))
+ * @returns {Promise}
+ */
+Yamaha.prototype.setVolumeTo = function (to) {
+    var command = this.createActiveZoneCommand('PUT', '<Volume><Lvl><Val>' + to + '</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume>');
     return this.SendXMLToReceiver(command);
 };
 
+/**
+ *
+ * @param by {Number} increase volume of the active zone by the given value (this value will be summed to the current volume)
+ * @returns {Promise}
+ */
+Yamaha.prototype.volumeUp = function (by) {
+    return this.adjustVolumeBy(by);
+};
+
+/**
+ *
+ * @param by {Number} decrease volume of the active zone by the given value (this value will be deducted to the current volume)
+ * @returns {Promise}
+ */
+Yamaha.prototype.volumeDown = function (by) {
+    return this.adjustVolumeBy(-by);
+};
+
+/**
+ *
+ * @param by {Number} change volume of the active zone by the given value (this value, either positive or negative) will be summed to the current volume)
+ * @returns {Promise}
+ */
+Yamaha.prototype.adjustVolumeBy = function (by) {
+    var self = this;
+    //what we need to do here is get the basic info about the receiver, then change the volume by the given number
+    //number can be either positive or negative
+    if (typeof by == 'string' || by instanceof String) {
+        by = parseInt(by);
+    }
+    //return instantly resolved promise for chaining if the delta is zero or NaN (or any other bad value)
+    if(!by || !by instanceof Number) {
+        return new Promise().resolve();
+    }
+
+    return this.getBasicInfo().then(function (basicInfo) {
+        return self.setVolumeTo(basicInfo.getVolume() + by);
+    });
+};
+
+/**
+ * Sets the input of the active zone
+ * @param {String} to name of input, e.g. "NET RADIO"
+ */
+Yamaha.prototype.setInputTo = function (to) {
+    var command;
+    command = this.createActiveZoneCommand('PUT', '<Input><Input_Sel>' + to + '</Input_Sel></Input>');
+    return this.SendXMLToReceiver(command);
+};
+
+/**
+ * Gets the basic info about the currenly active zone
+ * @returns {Promise}
+ */
+Yamaha.prototype.getBasicInfo = function () {
+    var _self = this,
+        command = this.createZoneCommand('GET', '<Basic_Status>GetParam</Basic_Status>');
+    return this.SendXMLToReceiver(command).then(function (basicInfo) {
+        return basicInfo.YAMAHA_AV[0][_self.activeZoneName()][0].Basic_Status[0];
+    });
+};
+
+/**
+ * Gets the volume level (from -800 to 165)
+ * @returns {Promise}
+ */
+Yamaha.prototype.getVolume = function () {
+    return this.getBasicInfo().then(function (basicStatus) {
+        return parseInt(basicStatus.Volume[0].Lvl[0].Val[0]._text);
+    })
+};
+
+/**
+ * Gets the mute state (boolean)
+ * @returns {Promise}
+ */
+Yamaha.prototype.isMuted = function () {
+    return this.getBasicInfo().then(function (basicStatus) {
+        return parseInt(basicStatus.Volume[0].Mute[0]._text !== "Off");
+    })
+};
+
+/**
+ * Gets the zone state (boolean)
+ * @returns {Promise}
+ */
+Yamaha.prototype.isOn = function () {
+    return this.getBasicInfo().then(function (basicStatus) {
+        return parseInt(basicStatus.Power_Control[0].Power[0]._text === "On");
+    })
+};
+
+/**
+ * Gets current input name (string)
+ * @returns {Promise}
+ */
+Yamaha.prototype.getCurrentInput = function () {
+    return this.getBasicInfo().then(function (basicStatus) {
+        return parseInt(basicStatus.Input[0].Input_Sel[0]._text);
+    });
+};
+
+/**
+ * Gets pure direct state (boolean)
+ * @returns {Promise}
+ */
+Yamaha.prototype.isPureDirectEnabled = function () {
+    return this.getBasicInfo().then(function (basicStatus) {
+        return parseInt(basicStatus.Sound_Video[0].Pure_Direct[0].Mode[0]._text === "On");
+    });
+};
+
+/**
+ * Gets party mode state (boolean)
+ * @returns {Promise}
+ */
+Yamaha.prototype.isPartyModeEnabled = function () {
+    return this.getBasicInfo().then(function (basicStatus) {
+        try {
+            return basicStatus.Party_Info[0]._text === "On";
+        }
+            //this means getting the property of undefined, so the party mode is not supported by the receiver
+        catch (err) {
+            return false;
+        }
+    });
+};
+
+/**
+ * Get common system info (Object)
+ * @returns {Promise}
+ */
+Yamaha.prototype.getSystemConfig = function () {
+    var command = this.createCommand('GET', 'System', '<Config>GetParam</Config>');
+    return this.SendXMLToReceiver(command);
+};
+
+
+/**
+ * Get all available inputs for this receiver.
+ * We should probably use them to check input availability before changing inputs
+ * @returns {Promise}
+ */
 Yamaha.prototype.getAvailableInputs = function () {
-    this.getSystemConfig().then(function (result) {
+    this.getSystemConfig().then(function (systemConfig) {
         var inputs = [],
-        inputsXML = result.YAMAHA_AV.System[0].Config[0].Name[0].Input[0];
+        inputsXML = systemConfig.YAMAHA_AV.System[0].Config[0].Name[0].Input[0];
         for (var prop in inputsXML) {
             if(inputsXML.hasOwnProperty(prop)) {
                 inputs.push(inputsXML[prop][0]);
@@ -200,15 +362,27 @@ Yamaha.prototype.getAvailableInputs = function () {
     });
 };
 
-Yamaha.prototype.selectListItem = function (listname, number) {
-    var command = '<YAMAHA_AV cmd="PUT"><' + listname + '><List_Control><Direct_Sel>Line_' + number + '</Direct_Sel></List_Control></' + listname + '></YAMAHA_AV>';
+/**
+ * Select one item from the list.
+ * Yamaha receivers currently support items from 1 to 8
+ * @param input - input to get the list from
+ * @param number - # of item (1 to 8)
+ * @returns {Promise}
+ */
+Yamaha.prototype.selectItem = function (input, number) {
+    var command = this.createCommand('PUT', input, '<List_Control><Direct_Sel>Line_' + number + '</Direct_Sel></List_Control>');
     return this.SendXMLToReceiver(command);
 };
 
-Yamaha.prototype.getList = function (name) {
-    var command = '<YAMAHA_AV cmd="GET"><' + name + '><List_Info>GetParam</List_Info></' + name + '></YAMAHA_AV>';
+/**
+ * Get the list of items for one of the inputs
+ * @param input - input name
+ * @returns {Promise}
+ */
+Yamaha.prototype.getList = function (input) {
+    var command = this.createCommand('GET', input, '<List_Info>GetParam</List_Info>');
     return this.SendXMLToReceiver(command).then(function (result) {
-        return enrichListInfo(result, name);
+        return enrichListInfo(result, input);
     });
 };
 
@@ -276,38 +450,13 @@ Yamaha.prototype.whenMenuReady = function (name) {
     return self.when("isMenuReady", name, true);
 };
 
-/**
- * long polling of any request made to the receiver
- * @param YamahaCall - the metod name to be called
- * @param parameter - argument to pass to the above method
- * @param expectedReturnValue - stop polling when this value meets our requirements
- * @returns {Promise}
- */
-Yamaha.prototype.when = function (YamahaCall, parameter, expectedReturnValue) {
-    var self = this;
-    return new Promise(function (resolve, reject) {
-        var tries = 0;
-        var interval = setInterval(function () {
-            self[YamahaCall](parameter).then(function (result) {
-                if (result == expectedReturnValue) {
-                    clearInterval(interval);
-                    resolve();
-                }
-                tries++;
-                if (tries > 40) reject("Timeout");
-            });
-
-        }, 500);
-    });
-};
-
 Yamaha.prototype.selectUSBListItem = function (number) {
-    return this.selectListItem("USB", number);
+    return this.selectItem("USB", number);
 };
 
 Yamaha.prototype.selectWebRadioListItem = function (number) {
     var _self = this;
-    return this.selectListItem("NET_RADIO", number).then(function(result) {
+    return this.selectItem("NET_RADIO", number).then(function(result) {
         return _self.getWebRadioList();
     });
 };
